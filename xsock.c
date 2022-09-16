86cba57d9458180d4c0c4ffdb3ee72ba59959cf0
speedyb0y <speedyb0y.rev0ltz@gmail.com>
2022-09-16 00:53:32 -0300

    SUPER HOSTS

--- xsock.c
+++ xsock.c
@@ -559,7 +559,7 @@ static netdev_tx_t xsock_out (sk_buff_s* const skb, net_device_s* const dev) {
     const uint ipSize = skb->len + sizeof(wire_hash_t);
 
     // DON'T ALLOW INTERFERENCE FROM IPV6, ICMP, WRONG ADDRESSES/PORTS
-    if (PTR(wire) < SKB_HEAD(skb)) {
+    if (WIRE_ETH(wire) < SKB_HEAD(skb)) {
         printk("OUT: DROP: SKB START\n");
         goto drop;
     }
