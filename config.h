c22085daadd5add365d66391cb5b8f93995acfe0
speedyb0y <speedyb0y.rev0ltz@gmail.com>
2022-09-15 22:37:52 -0300

    SUPER HOSTS

--- config.h
+++ config.h
@@ -23,9 +23,9 @@
 #define IP_1_D 20
 #define IP_2_C 1
 #define IP_2_D 20
-#define ITFC0 "eth0"
+#define ITFC0 "eth0xxx"
 #define ITFC1 "eth0xx"
-#define ITFC2 "eth0xxx"
+#define ITFC2 "eth0"
 #define MAC0 "\xd0\x50\x99\xae\xde\x92"
 #define MAC1 "\xd0\x50\x99\xae\xde\x92"
 #define MAC2 "\xd0\x50\x99\xae\xde\x92"
